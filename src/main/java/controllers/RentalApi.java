559c9d141bae65b455b67d665fd3d0ba77cbe417
dio-quincarDev <dio-quincar@outlook.com>
2025-01-26 18:01:36 -0500

    add rentals

---
+++ src/main/java/controllers/RentalApi.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import commons.constants.ApiPathConstants;
+import commons.entities.Rental;
+
+@RequestMapping(ApiPathConstants.V1_ROUTE + "/rentals")
+public interface RentalApi {
+	@PostMapping
+	ResponseEntity<Rental>createRental(@RequestBody Rental rental);
+	
+	@GetMapping
+	ResponseEntity<List<Rental>>getAllRentals();
+	
+	@GetMapping("/{id}")
+	ResponseEntity<Rental>getRentalById(@PathVariable Long id, @RequestBody Rental rental);
+	
+    @PutMapping("/{id}")
+	ResponseEntity<Rental> updateRental(@PathVariable Long id, @RequestBody Rental rental);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deleteRental(@PathVariable Long id);
+	
+
+}
--- src/main/java/service/RentalService.java
+++ src/main/java/service/RentalService.java
@@ -1,5 +1,16 @@
 package service;
 
+import java.util.List;
+import java.util.Optional;
+
+import commons.entities.Rental;
+
 public interface RentalService {
+	
+	List<Rental>findAllRentals();
+	Optional<Rental>findRentalById(Long id);
+	Rental createRental (Rental rental);
+	Rental updateRental(Long id, Rental rental);
+	void deleteRental(Long id);
 
 }
